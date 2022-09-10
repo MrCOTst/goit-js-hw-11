371497b79fd0356847950f1d6bbf63033a963397
MrCOTst <mypostgg@gmail.com>
2022-09-10 21:58:35 +0300

    1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+//  Ініціюємо кнопки та екран (боді) //
 
+  const body = document.body;
+  let timerId = null;
+  const refs = {
+    startBtn: document.querySelector("button[data-start]"),
+    stopBtn: document.querySelector("button[data-stop]"),
+  };
+
+// Підключаємо слухачів натискання на кнопки //
+
+  refs.startBtn.addEventListener("click", createColorBody);
+  refs.stopBtn.addEventListener("click", stopCreate);
+
+  // Функція генерації та зміни кольору єкрана //
+  function createColorBody() {
+    timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    body.style.backgroundColor = randomColor;
+      }, 1000);
+      refs.startBtn.setAttribute('disabled', 'disabled');
+      refs.startBtn.style.backgroundColor = 'rgba(255, 255, 255, 0.3)'
+      console.log("Enabled was removed from button Start");
+  }
+
+  // Функція зупинки зміни кольорів екрану //
+  
+    function stopCreate() {
+    clearInterval(timerId);
+    refs.startBtn.removeAttribute('disabled', 'disabled');
+    refs.startBtn.style.backgroundColor = '#2196f3';
+    console.log(`Interval with id ${timerId} has stopped!`);
+  }
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
